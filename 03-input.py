b8f668d0012a569a69bb5ece9763fad4551f5bd6
joserenatocarmo-ops <joserenatocarmo@gmail.com>
2026-08-29 13:39:08 -0300

    up4

--- 03-input.py
+++ 03-input.py
@@ -5,7 +5,7 @@ faturamento = float(faturamento)  # 800
 
 print(faturamento)
 
-custo = 700
+custo = 800
 
 lucro = faturamento - custo
 print(f"O faturamento foi de {faturamento} e o lucro foi de {lucro}")
